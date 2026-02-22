b1939c86d8186cb07612de4099ec34aeed74b2e3
pmaurogut <pmaurogut@gmail.com>
2026-02-22 18:59:05 +0100

    fixed hats 1

--- utils.R
+++ utils.R
@@ -546,7 +546,7 @@ confint_plot<-function(estimates, var, par_int,conf){
   means$x_max <- means$mean + conf*means$sd
   
   means$rel_error <- round(100*conf*means$sd/means$mean,1)
-  means$label_sd1 <- paste("'hat(sd)(hat(mu)[p])==",round(means$sd*sqrt(means$n),1),sep="")
+  means$label_sd1 <- paste("hat(sd)(hat(mu)[p])==",round(means$sd*sqrt(means$n),1),sep="")
   means$label_sdn<- paste("hat(sd)(hat(mu)[final])==",round(means$sd,1),sep="")
   means$label_rel_error<- paste("hat(epsilon)['final']==",round(means$rel_error,1),"~'%'",sep="")
   
